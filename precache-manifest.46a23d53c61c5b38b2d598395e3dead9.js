640c4f0a48d586c92bba06d083a52c0afd603c4a
Woody <woody.feng@hotmail.com>
2019-09-21 18:16:19 +0800

    Updates

---
+++ precache-manifest.46a23d53c61c5b38b2d598395e3dead9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f3c59bc7b45df5a81bde42ce2ed794",
+    "url": "/spin/index.html"
+  },
+  {
+    "revision": "623c8842e41130db9614",
+    "url": "/spin/static/js/2.0e6d7046.chunk.js"
+  },
+  {
+    "revision": "b3c896a3ee4ee8c16682",
+    "url": "/spin/static/js/main.e51e20bc.chunk.js"
+  },
+  {
+    "revision": "dc932f3a73107bd83d64",
+    "url": "/spin/static/js/runtime-main.c381b7f8.js"
+  }
+]);
